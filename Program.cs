6fc8b357b9f6bf1a82c7f98e9c8afd5d350ff297
Dan <me@danielholcomb.com>
2022-11-20 09:36:42 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).AddEnvironmentVariables();
+builder.Configuration.AddEnvironmentVariables().Build();
 
 var app = builder.Build();
 
