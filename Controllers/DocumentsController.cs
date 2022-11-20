0708d9fa5b388754b271836362ac7e5c93ff93df
Dan <me@danielholcomb.com>
2022-11-20 14:04:24 -0700

    add basic jwt authentication

--- Controllers/DocumentsController.cs
+++ Controllers/DocumentsController.cs
@@ -4,6 +4,11 @@ using Google.Apis.Services;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Analytics.v3;
 using Google.Apis.Drive.v2;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 
 namespace Design.Documents.Controllers
 {
@@ -20,6 +25,7 @@ namespace Design.Documents.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> Get()
         {
             try
@@ -51,12 +57,35 @@ namespace Design.Documents.Controllers
                 var request = docsService.Documents.Get("1Emy2vaxH6Blnw0BL-cr5EcsU8AKIM4GvOWJTn7SrKDg");
                 return Ok(files);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
         }
 
+        [HttpGet]
+        [Route("Token")]
+        public async Task<IActionResult> Token()
+        {
+            return Ok(GenerateJwtToken());
+        }
+
+        private string GenerateJwtToken()
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.GetSection("Jwt:Key").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", "design") }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _config.GetSection("Jwt:Issuer").Value,
+                Audience = _config.GetSection("Jwt:Audience").Value,
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
 
     }
 }
